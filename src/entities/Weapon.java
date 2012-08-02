1d589789a716fe6796e629d20ca9e89eb31f12f1
Karol Gliniecki <karol.gliniecki@googlemail.com>
2012-08-02 21:28:43 +0200

    started to implement overheating

--- src/entities/Weapon.java
+++ src/entities/Weapon.java
@@ -1,7 +1,5 @@
 package entities;
 
-import java.util.ArrayList;
-
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
@@ -10,11 +8,13 @@ import org.newdawn.slick.SlickException;
 public class Weapon {
 
 	
-	ArrayList<Bullet> bullets;
 	private Image img;
+	private int heatLoad = 100;
+	private int heatIncrease = 20;
+	private float heatDecay = 0.001f;
+	private int heat = 0;
 	
 	public Weapon() {
-		bullets = new ArrayList<Bullet>();
 		try {
 			img = new Image("res/weapon.png");
 		} catch (SlickException e) {
@@ -23,12 +23,13 @@ public class Weapon {
 	}
 	
 	
-	public ArrayList<Bullet> shoot(Ship owner) {
-		
-		owner.bullets.add(new Bullet((int) owner.getX()+((owner.getWidth()/ 2)), owner.getY() - 10, 2, 10, 1, owner));
-		bullets.add(new Bullet((int) owner.getX()+((owner.getWidth()/ 2)), owner.getY() - 10, 2, 10, 1, owner));
-		
-		return bullets;
+	public void shoot(Ship owner) {
+		owner.getBulletLoader().add(new Bullet((int) owner.getX()+((owner.getWidth()/ 2)), owner.getY() - 10, 2, 10, 1, owner));
+//		bullets.add(new Bullet((int) owner.getX()+((owner.getWidth()/ 2)), owner.getY() - 10, 2, 10, 1, owner));
+		if(heat+heatIncrease <= heatLoad)
+			heat += heatIncrease;
+		else if(heat+heatIncrease >heatLoad)
+			heat = heatLoad;
 	}
 
 
@@ -44,6 +45,42 @@ public class Weapon {
 			e.printStackTrace();
 		}
 	}
+	
+	public int getHeatLoad() {
+		return heatLoad;
+	}
+
+
+	public void setHeatLoad(int heatLoad) {
+		this.heatLoad = heatLoad;
+	}
+
+
+	public int getHeatIncrease() {
+		return heatIncrease;
+	}
+
+
+	public void setHeatIncrease(int heatIncrease) {
+		this.heatIncrease = heatIncrease;
+	}
+
+
+	public int getHeat() {
+		return heat;
+	}
+
+
+	public void setHeat(int heat) {
+		this.heat = heat;
+	}
+	
+	public void decreaseHeat() {
+		heat -= heatDecay;
+		if(heat < 0)
+			heat = 0;
+	}
+
 	
 	
 	
